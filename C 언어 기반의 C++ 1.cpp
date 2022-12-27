c4976cb9ec28d7de48601c06c69d1e838e5b6345
Minsu-Jung1999 <96643903+Minsu-Jung1999@users.noreply.github.com>
2022-12-27 08:59:25 -0800

    12-26

---
+++ C 언어 기반의 C++ 1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int BoxVolume(int length, int width, int height) {
+	return length * width * height;
+}
+
+int BoxVolume(int length, int width) {
+	return length * width;
+}
+
+int BoxVolume(int length) {
+	return length;
+}
+
+
+int main()
+{
+	cout << BoxVolume(3,3,3)<<endl;
+	cout << BoxVolume(5,5)<<endl;
+	cout << BoxVolume(7)<<endl;
+}
+
